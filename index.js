103afe3e74ce815bc2a23f232ec7b36f9df2ddf3
Elyse Burmester <burmesterelyse@gmail.com>
2019-06-14 17:28:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const baseURL = 'https://api.github.com';
 const user = '<YOUR_USERNAME>';
-
+const token = 'YOUR_TOKEN_HERE';
+ 
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+  
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
